2f7b4d51b4052687b682b5d557899fac89908aac
A6e <A6E@users.noreply.github.com>
2021-03-04 22:06:23 -0500

    Add placeholder content

--- cmps262-team4/src/App.js
+++ cmps262-team4/src/App.js
@@ -9,6 +9,20 @@ function App() {
           <a href="/login">Login</a>
           <a href="/create">Create an Account</a>
       </header>
+        <div className="posts">
+            <div className="post">
+                <div className="post-header">TestUser • Feb 24, 2021 at 2:23 PM</div>
+                <div className="post-content">Test post 123</div>
+            </div>
+            <div className="post">
+                <div className="post-header">TestUser2 • March 24, 2021 at 2:23 PM</div>
+                <div className="post-content">T1232432 qwe est post 123</div>
+            </div>
+            <div className="post">
+                <div className="post-header">252933 • May 24, 2021 at 2:23 PM</div>
+                <div className="post-content">T123 est post 123. long content T123 est post 123. long content T123 est post 123. long content T123 est post 123. long content T123 est post 123. long content T123 est post 123. long content </div>
+            </div>
+        </div>
     </div>
   );
 }
