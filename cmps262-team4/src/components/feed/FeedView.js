ca5b2f7c3ee66d81735b47011f43aee753f7ad18
A6e <A6E@users.noreply.github.com>
2021-03-31 20:20:16 -0400

    Add styled feed view

---
+++ cmps262-team4/src/components/feed/FeedView.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import {MOCK_POST_COUNT, util} from "../Util";
+import { name, lorem } from "faker";
+import Post from "./Post";
+
+
+export default function FeedView () {
+    const [posts, setPosts] = useState(null);
+    useEffect(_ => {
+        // normally request posts from server
+        // create fake posts for now as there is no server/db
+        // fetchPosts = async _ => { }; await fetchPosts();
+
+        let mockPosts = [];
+        for (let i = 0; i <= MOCK_POST_COUNT; i++) {
+            //let name = util.generateFakeName();
+            let mockName = name.findName();
+            let username = mockName.toLowerCase().split(" ").join("-");
+
+            let wordCount = Math.floor(Math.random() * 50) + 5;
+            let text = lorem.words(wordCount);
+            let date = new Date();
+            if (i >= date.getMinutes())
+                date.setHours(date.getHours() - 1);
+            date.setMinutes(i);
+
+            mockPosts.push({
+                id: wordCount,
+                author: {
+                    name: mockName,
+                    username: username
+                },
+                text: text,
+                date: date
+            });
+        }
+
+        setPosts(mockPosts);
+    }, []);
+  return (
+      <>
+          {posts && posts.map((post, key) => {
+              return <Post post={post} key={key} />
+          })}
+      </>
+  )
+};
+FeedView.__TITLE__ = "Feed";
---
+++ cmps262-team4/src/components/feed/Post.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Moment from 'react-moment';
+
+export default function Post ({ post }) {
+    return (
+        <article className="media">
+            <figure className="media-left">
+                <p className="image is-64x64">
+                    <img src="https://via.placeholder.com/128x128" />
+                </p>
+            </figure>
+            <div className="media-content">
+                <div className="content">
+                    <p>
+                        <strong>{post.author.name}</strong> <small>@{post.author.username}</small>
+                        {" "}&mdash; <small><Moment fromNow withTitle>{post.date}</Moment></small>
+                        <br />
+                        {post.text}
+                    </p>
+                </div>
+                <nav className="level is-mobile">
+                    <div className="level-left">
+                        <a className="level-item">
+                            <span className="icon is-small"><i className="fa fa-reply"></i></span>
+                        </a>
+                        <a className="level-item">
+                            <span className="icon is-small"><i className="fa fa-retweet"></i></span>
+                        </a>
+                        <a className="level-item">
+                            <span className="icon is-small"><i className="fa fa-heart"></i></span>
+                        </a>
+                    </div>
+                </nav>
+            </div>
+            <div className="media-right">
+                <button className="delete"></button>
+            </div>
+        </article>
+    );
+}
