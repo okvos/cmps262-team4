1229397cfe7311a5261324675f408e8a9ba44c19
A6e <A6E@users.noreply.github.com>
2021-04-13 13:51:30 -0400

    Add login, and reformat files

--- cmps262-team4/src/components/Util.js
+++ cmps262-team4/src/components/Util.js
@@ -1,27 +1,23 @@
-const FAKE_FIRST_NAMES = [
-    "John",
-    "Joe",
-    "Mary",
-    "Sally",
-    "Tom"
-];
+const FAKE_FIRST_NAMES = ["John", "Joe", "Mary", "Sally", "Tom"];
 
-const FAKE_LAST_NAMES = [
-    "Smith",
-    "Doe",
-    "Joseph",
-    "Brown",
-    "Thompson",
-];
+const FAKE_LAST_NAMES = ["Smith", "Doe", "Joseph", "Brown", "Thompson"];
 
 class Util {
-    constructor() { }
+  constructor() {
+    this.username = null;
+  }
 
-    generateFakeName () {
-        let firstName = FAKE_FIRST_NAMES[Math.floor(Math.random() * FAKE_FIRST_NAMES.length)];
-        let lastName = FAKE_LAST_NAMES[Math.floor(Math.random() * FAKE_LAST_NAMES.length)];
-        return `${firstName} ${lastName}`;
-    }
+  generateFakeName() {
+    let firstName =
+      FAKE_FIRST_NAMES[Math.floor(Math.random() * FAKE_FIRST_NAMES.length)];
+    let lastName =
+      FAKE_LAST_NAMES[Math.floor(Math.random() * FAKE_LAST_NAMES.length)];
+    return `${firstName} ${lastName}`;
+  }
+
+  isLoggedIn() {
+    return this.username !== null;
+  }
 }
 
 export const util = new Util();
