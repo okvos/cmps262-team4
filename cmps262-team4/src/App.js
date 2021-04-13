a47deb31f887e5ca6c94aee9b4dd0887cd6a36a0
A6e <A6E@users.noreply.github.com>
2021-04-13 13:51:30 -0400

    Add login, and reformat files

--- cmps262-team4/src/App.js
+++ cmps262-team4/src/App.js
@@ -1,23 +1,26 @@
-import 'bulma/css/bulma.css';
+import "bulma/css/bulma.css";
 import Navigation from "./components/Navigation";
+import { util } from "./components/Util";
 
-export function ViewManager ({ view }) {
-    let View = view; // components need to be capitalized
-    return (
-        <section className="section">
-            {view.__TITLE__ && <h1 className="title">{view.__TITLE__}</h1>}
-            <div className="box">
-                <View />
-            </div>
-
-        </section>
-    )
+export function ViewManager({ view }) {
+  let View = view; // components need to be capitalized
+  return (
+    <section className="section">
+      {view.__TITLE__ && <h1 className="title">{view.__TITLE__}</h1>}
+      <div className="box">
+        <View />
+      </div>
+    </section>
+  );
 }
 
 function App() {
+  // check for username to see if we're logged in, and then set it in Util
+  let username = window.localStorage.getItem("username");
+  util.username = username;
   return (
     <div className="App">
-        <Navigation />
+      <Navigation />
     </div>
   );
 }
--- cmps262-team4/src/components/feed/FeedView.js
+++ cmps262-team4/src/components/feed/FeedView.js
@@ -1,48 +1,47 @@
 import React, { useEffect, useState } from "react";
-import {MOCK_POST_COUNT, util} from "../Util";
+import { MOCK_POST_COUNT, util } from "../Util";
 import { name, lorem } from "faker";
 import Post from "./Post";
 
+export default function FeedView() {
+  const [posts, setPosts] = useState(null);
+  useEffect((_) => {
+    // normally request posts from server
+    // create fake posts for now as there is no server/db
+    // fetchPosts = async _ => { }; await fetchPosts();
 
-export default function FeedView () {
-    const [posts, setPosts] = useState(null);
-    useEffect(_ => {
-        // normally request posts from server
-        // create fake posts for now as there is no server/db
-        // fetchPosts = async _ => { }; await fetchPosts();
+    let mockPosts = [];
+    for (let i = 0; i <= MOCK_POST_COUNT; i++) {
+      //let name = util.generateFakeName();
+      let mockName = name.findName();
+      let username = mockName.toLowerCase().split(" ").join("-");
 
-        let mockPosts = [];
-        for (let i = 0; i <= MOCK_POST_COUNT; i++) {
-            //let name = util.generateFakeName();
-            let mockName = name.findName();
-            let username = mockName.toLowerCase().split(" ").join("-");
+      let wordCount = Math.floor(Math.random() * 50) + 5;
+      let text = lorem.words(wordCount);
+      let date = new Date();
+      if (i >= date.getMinutes()) date.setHours(date.getHours() - 1);
+      date.setMinutes(i);
 
-            let wordCount = Math.floor(Math.random() * 50) + 5;
-            let text = lorem.words(wordCount);
-            let date = new Date();
-            if (i >= date.getMinutes())
-                date.setHours(date.getHours() - 1);
-            date.setMinutes(i);
+      mockPosts.push({
+        id: wordCount,
+        author: {
+          name: mockName,
+          username: username,
+        },
+        text: text,
+        date: date,
+      });
+    }
 
-            mockPosts.push({
-                id: wordCount,
-                author: {
-                    name: mockName,
-                    username: username
-                },
-                text: text,
-                date: date
-            });
-        }
-
-        setPosts(mockPosts);
-    }, []);
+    setPosts(mockPosts);
+  }, []);
   return (
-      <>
-          {posts && posts.map((post, key) => {
-              return <Post post={post} key={key} />
-          })}
-      </>
-  )
-};
+    <>
+      {posts &&
+        posts.map((post, key) => {
+          return <Post post={post} key={key} />;
+        })}
+    </>
+  );
+}
 FeedView.__TITLE__ = "Feed";
